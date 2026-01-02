b79a34d78c4be2bba4bba80c1a3fcda53b6d0249
Dhanush51 <kotegarkumbledhanush@example.com>
2026-01-02 20:25:56 +0530

    Fixed Invoice Count in Dashboard3

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton, } from '@/app/ui/skeletons';
 
+export const revalidate = 0;
  
 export default async function Page() {
   
